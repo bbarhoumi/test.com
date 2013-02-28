76a607f555f58823a49f9076a950511a208a2d6e
brahim.barhoumi@esprit.tn <brahim.barhoumi@esprit.tn>
2013-02-28 01:19:23 +0100

    first-commit

---
+++ commentaire.php
@@ -0,0 +1,30 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta  charset="UTF-8">
+        <title> les commentaires en PHP</title>
+    </head>
+    <body>
+        <?php 
+        echo 'je suis ici';
+        
+        
+        ?>
+        <?php
+        
+        /**
+         *la somme de deux chiffres
+         * @param int $a
+         * @param int $b
+         * @return int 
+         */
+        function sommezerzrzrz($a,$b){
+            return $a+$b;
+        }
+      echo sommezerzrzrz(3232, 2154);
+        
+        
+        ?>
+    </body>
+</html>
